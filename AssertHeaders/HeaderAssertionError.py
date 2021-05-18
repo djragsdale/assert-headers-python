dc26a1c465f8fbf904dd89dcd6dbf04b778f51c1
David Ragsdale <me@davidjragsdale.com>
2021-05-18 09:25:00 -0400

    Fix attribute retrieval in map lambda

--- AssertHeaders/HeaderAssertionError.py
+++ AssertHeaders/HeaderAssertionError.py
@@ -1,5 +1,5 @@
 class HeaderAssertionError(Exception):
     def __init__(self, errors):
         self.errors = errors
-        self.message = "The following header errors occurred:\n- " + "\n- ".join(map(lambda error: error.message, errors))
+        self.message = "The following header errors occurred:\n- " + "\n- ".join(map(lambda error: error.get("message"), errors))
         super().__init__(self.message)
